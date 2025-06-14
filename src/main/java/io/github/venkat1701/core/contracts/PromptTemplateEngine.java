e8e61db23405efba35f6c281830353e1ac8d3232
venkat1701 <thejeastdev@gmail.com>
2025-06-15 01:18:22 +0530

    package refactored

--- src/main/java/io/github/venkat1701/core/contracts/PromptTemplateEngine.java
+++ src/main/java/io/github/venkat1701/core/contracts/PromptTemplateEngine.java
@@ -1,6 +1,6 @@
 package io.github.venkat1701.core.contracts;
 
-import io.github.venkat1701.core.payloads.ResearchContext;
+import io.github.venkat1701.reasoning.context.ResearchContext;
 import io.github.venkat1701.core.payloads.ResearchPromptConfig;
 
 public interface PromptTemplateEngine {
