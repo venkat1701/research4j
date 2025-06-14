2bb9c338d9c717556b853d5aef62dbf897b80ad7
venkat1701 <thejeastdev@gmail.com>
2025-06-15 01:17:56 +0530

    deleted old payloads

--- src/main/java/io/github/venkat1701/core/contracts/CitationFetcher.java
+++
@@ -1,9 +0,0 @@
-package io.github.venkat1701.core.contracts;
-
-import java.util.List;
-
-import io.github.venkat1701.core.payloads.CitationSource;
-
-public interface CitationFetcher {
-    List<CitationSource> fetchCitation(String query);
-}
--- src/main/java/io/github/venkat1701/core/payloads/CitationSource.java
+++
@@ -1,5 +0,0 @@
-package io.github.venkat1701.core.payloads;
-
-public record CitationSource(String title, String url, String rawContent) {
-
-}
