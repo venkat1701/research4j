477400c047c44e81cdef83114ebd8d3bda2c3570
venkat1701 <thejeastdev@gmail.com>
2025-06-14 17:59:59 +0530

    converted to record

--- src/main/java/io/github/venkat1701/core/payloads/CitationSource.java
+++ src/main/java/io/github/venkat1701/core/payloads/CitationSource.java
@@ -1,17 +1,5 @@
 package io.github.venkat1701.core.payloads;
 
-public class CitationSource {
-    private final String title;
-    private final String url;
-    private final String rawContent;
+public record CitationSource(String title, String url, String rawContent) {
 
-    public CitationSource(String title, String url, String rawContent) {
-        this.title = title;
-        this.url = url;
-        this.rawContent = rawContent;
-    }
-
-    public String getTitle() { return title; }
-    public String getUrl() { return url; }
-    public String getRawContent() { return rawContent; }
 }
--- src/main/java/io/github/venkat1701/core/payloads/ResearchPromptConfig.java
+++ src/main/java/io/github/venkat1701/core/payloads/ResearchPromptConfig.java
@@ -2,22 +2,6 @@ package io.github.venkat1701.core.payloads;
 
 import io.github.venkat1701.core.enums.OutputFormat;
 
-public class ResearchPromptConfig {
-    private final String userPrompt;
-    private final String systemInstruction;
-    private final Class<?> outputType;
-    private final OutputFormat outputFormat;
-
-    public ResearchPromptConfig(String userPrompt, String systemInstruction, Class<?> outputType, OutputFormat outputFormat) {
-        this.userPrompt = userPrompt;
-        this.systemInstruction = systemInstruction;
-        this.outputType = outputType;
-        this.outputFormat = outputFormat;
-    }
-
-    public String getUserPrompt() { return userPrompt;}
-    public String getSystemInstruction() { return systemInstruction;}
-    public Class<?> getOutputType() { return outputType;}
-    public OutputFormat getOutputFormat() { return outputFormat;}
+public record ResearchPromptConfig(String userPrompt, String systemInstruction, Class<?> outputType, OutputFormat outputFormat) {
 
 }
