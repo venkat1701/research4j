d9640d0ca350c805f12c62a2e46b0541af46ff04
venkat1701 <thejeastdev@gmail.com>
2025-06-15 01:18:45 +0530

    purged ReasoningStrategy to the most recent one

--- src/main/java/io/github/venkat1701/core/contracts/ReasoningStrategy.java
+++
@@ -1,7 +0,0 @@
-package io.github.venkat1701.core.contracts;
-
-import io.github.venkat1701.core.payloads.ResearchContext;
-
-public interface ReasoningStrategy {
-    ResearchContext reason(ResearchContext context);
-}
